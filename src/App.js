65f73516978164cd114ecacadc30efa1ebabc654
Ruslan Pozov <ruslanpozov@MacBook-Air-Ruslan.local>
2021-10-05 19:32:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Car from './Car/Car'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const stasik = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016},
+            {name: 'mazda', year: 2010}
+        ],
+        pageTitle: 'React '
+    }
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [pageTitle, setPageTitle] = useState(stasik.pageTitle);
+
+    const changeTitleHandler = () => {
+        setPageTitle(stasik.pageTitle + ' xyu')
+
+    }
+
+    const divStyle = {
+        textAlign: 'center'
+    }
+
+    const cars = stasik.cars
+        return (
+            <div style={divStyle}>
+                <h1>{pageTitle}</h1>
+
+                <button onClick={changeTitleHandler}>Change title</button>
+
+                <Car
+                    name={cars[0].name}
+                     year={cars[0].year} />
+                <Car
+                    name={cars[1].name}
+                    year={cars[1].year} />
+                <Car
+                    name={cars[2].name}
+                    year={cars[2].year} />
+            </div>
+        );
 }
+
 
 export default App;
